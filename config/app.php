2edb290cbe60abcea8f4ba741550062ed923b1d9
Berk <berkcanli@hotmail.com>
2017-07-30 21:23:16 +0300

    Added laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Extra Packages
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
