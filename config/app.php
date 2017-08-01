27f2623cb8afcb149972af76ff9b22e5fd1eba4f
Berk <berkcanli@hotmail.com>
2017-08-01 17:33:47 +0300

    Resolved #12: Create repositories for all models

--- config/app.php
+++ config/app.php
@@ -231,6 +231,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Packages
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
